f5aea6cf17cc9f0be89f3582c356eb53525c7317
Silas Michael Knight <silasfelinus@gmail.com>
2020-11-19 01:39:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,8 +9,8 @@ def summon_captain_planet(array)# code an argument here
   new_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)# code an argument here
+  array.any?(|name| name.length > 4)
 end
 
 def find_the_cheese# code an argument here
