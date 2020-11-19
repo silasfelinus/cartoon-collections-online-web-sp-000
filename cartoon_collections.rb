133e9967dd153e1349a942b8e709dc60d0dda707
Silas Michael Knight <silasfelinus@gmail.com>
2020-11-19 01:53:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,12 @@ def long_planeteer_calls(array)# code an argument here
   array.any? {|name| name.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+cheese_types.each {|cheese| 
+  if array.include? (cheese)
+    cheese
+  end
+}
 end
